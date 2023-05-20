021d94c38f80309d0d206d610997c806927a1d99
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-20 18:37:29 -0500

    u

--- DataTable.tsx
+++ DataTable.tsx
@@ -18,73 +18,8 @@ export function DataTable(props: DataTableProps) {
 		const indexOfLastPage = currentPage * postsPerPage;
 		const indexOfFirstPage = indexOfLastPage - postsPerPage;
 		const currentPosts = props.dataArray.slice(indexOfFirstPage, indexOfLastPage);
-		// return currentPosts.map(
-		// 	(
-		// 		item: {
-		// 			userId: number;
-		// 			title: string;
-		// 			completed: boolean;
-		// 		},
-		// 		index: number,
-		// 	) => {
-		// 		return (
-		// 			<tbody key={index}>
-		// 				<tr>
-		// 					<td>{postsPerPage * (currentPage - 1) + index + 1}</td>
-		// 					<td>{item.userId}</td>
-		// 					<td>{item.title}</td>
-		// 					<td>{item.completed.toString()}</td>
-		// 				</tr>
-		// 			</tbody>
-		// 		);
-		// 	},
-		// );
-
-
-
-		// currentPosts.map((data, index) => {
-		// 	return (
-		// 		<tr class="border-2" key={index}>
-		// 			{keys.map((k) => {
-		// 				// console.log(k);
-		// 				console.log('<td class="border-2">{' + data[k as keyof typeof data] + '}</td>');
-		// 				return (
-		// 					<td class="border-2">{data[k as keyof typeof data]}</td>
-		// 				);
-		// 			})}
-		// 		</tr>
-		// 	);
-		// })
-
-
-
-		// return "jytej"
-
-
-
-		// return (<table>
-		// 	<tr>
-		// 		<th>Company</th>
-		// 		<th>Contact</th>
-		// 		<th>Country</th>
-		// 	</tr>
-		// 	<tr>
-		// 		<td>Alfreds Futterkiste</td>
-		// 		<td>Maria Anders</td>
-		// 		<td>Germany</td>
-		// 	</tr>
-		// 	<tr>
-		// 		<td>Centro comercial Moctezuma</td>
-		// 		<td>Francisco Chang</td>
-		// 		<td>Mexico</td>
-		// 	</tr>
-		// </table>)
-
 
 		return (
-			// <table>
-
-
 			currentPosts.map((data, index) => {
 				return (
 					<tr class="border-2" key={index}>
@@ -96,10 +31,7 @@ export function DataTable(props: DataTableProps) {
 					</tr>
 				);
 			})
-
-			// </table >
 		)
-
 	}
 
 
@@ -110,34 +42,17 @@ export function DataTable(props: DataTableProps) {
 
 	return (
 		<>
-			<div>
-				<table className="table align-items-center justify-content-center mb-0">
-
-					{showData()}
-				</table>
-
-			</div>
-			{/* <div>
-				<table class="border-2">
-					<tr class="border-2">
-						{keys.map((data, index) => {
-							return <th class="border-2">{keys[index]}</th>;
-						})}
-					</tr>
-
-					{props.dataArray.map((data, index) => {
-						return (
-							<tr class="border-2" key={index}>
-								{keys.map((k) => {
-									return (
-										<td class="border-2">{data[k as keyof typeof data]}</td>
-									);
-								})}
-							</tr>
-						);
+			<table className="table align-items-center justify-content-center mb-0">
+				<tr class="border-2">
+					{keys.map((data, index) => {
+						return <th class="border-2">{keys[index]}</th>;
 					})}
-				</table>
-			</div> */}
+				</tr>
+
+				{showData()}
+
+			</table>
+
 		</>
 	);
 }
