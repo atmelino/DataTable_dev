d53c8f578f90377dee9f270a8a81e8daaba3dac3
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-20 20:24:26 -0500

    u

--- DataTable.tsx
+++ DataTable.tsx
@@ -36,6 +36,49 @@ export function DataTable(props: DataTableProps) {
 
 	function showPagination() {
 		const totalPosts = props.dataArray.length;
+		const inactive = "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+		const active = "z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+
+		return (
+			<nav aria-label="Page navigation example">
+				<ul class="inline-flex items-center -space-x-px">
+					<li>
+						<a href="#" class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+							<span class="sr-only">Previous</span>
+							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+						</a>
+					</li>
+					<li>
+						<a href="#" class={inactive} >1</a>
+					</li>
+					<li>
+						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+					</li>
+					<li>
+						<a href="#" aria-current="page" class={active}			>3</a>
+
+					</li>
+					<li>
+						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+					</li>
+					<li>
+						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+					</li>
+					<li>
+						<a href="#" class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+							<span class="sr-only">Next</span>
+							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+
+	function showPagination2() {
+		const totalPosts = props.dataArray.length;
+		const inactive = "block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+		const active = "z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
 
 		for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
 			pageNumbers.push(i);
@@ -46,6 +89,92 @@ export function DataTable(props: DataTableProps) {
 			setcurrentPage(pageNumbers);
 			// showData();
 		};
+
+
+		return (
+			<nav aria-label="Page navigation example">
+				<ul class="inline-flex items-center -space-x-px">
+					<li>
+						<a href="#" class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+							<span class="sr-only">Previous</span>
+							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+						</a>
+					</li>
+					<li>
+						<a href="#" class={inactive} >1</a>
+					</li>
+					<li>
+						<a href="#" aria-current="page" class={active}			>3</a>
+					</li>
+
+
+					{/* {pageNumbers.map((number) => (
+						// console.log("currentPage" + currentPage + " number" + number);
+						<li
+							// class={active}
+							// class='border-red-200 bg-red-300'
+							// class='border-blue-200 bg-blue-300'
+
+							class={currentPage === number
+								? { active }
+								: { inactive }}
+						// class={1 === 1
+						// 	? 'border-blue-200 bg-blue-300'
+						// 	: 'border-red-200 bg-red-300'
+						// }
+						>
+							<button
+								onClick={() => pagination(number)}>
+								{number}
+							</button>
+						</li>
+					))
+					} */}
+
+
+
+					{pageNumbers.map((number, index) => {
+						console.log("currentPage" + currentPage + " number" + number);
+						return (
+							<li
+								class={currentPage === number
+									? active
+									: inactive}
+							>
+								<button
+									onClick={() => pagination(number)}>
+									{number}
+								</button>
+							</li>
+						)
+					})}
+
+
+					{/* {pageNumbers.map((value, index) => {
+						console.log(value);
+						return (
+							<tr class="border-2" key={index}>{value}
+								<td>Alfreds Futterkiste</td>
+								<td>{value}</td>
+								<td class="border-2" >{JSON.stringify(value)}</td>
+							</tr>)
+					})} */}
+
+
+
+					<li>
+						<a href="#" class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+							<span class="sr-only">Next</span>
+							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		);
+
+
+
+
 
 		// return (
 		// 	<nav>
@@ -66,53 +195,6 @@ export function DataTable(props: DataTableProps) {
 		// 	</nav>
 		// );
 
-
-
-		return (
-
-			<nav aria-label="Page navigation example">
-				<ul class="inline-flex items-center -space-x-px">
-					<li>
-						<a href="#" class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-							<span class="sr-only">Previous</span>
-							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-					</li>
-					<li>
-						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-					</li>
-					<li>
-						<a href="#" aria-current="page" class="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-					</li>
-					<li>
-						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-					</li>
-					<li>
-						<a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-					</li>
-					<li>
-						<a href="#" class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-							<span class="sr-only">Next</span>
-							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-						</a>
-					</li>
-				</ul>
-			</nav>
-		);
-
-
-
-
-
-
-
-
-
-
-
 	}
 
 
@@ -132,6 +214,7 @@ export function DataTable(props: DataTableProps) {
 				{showData()}
 				<div style={{ float: "right" }}>
 					{showPagination()}
+					{showPagination2()}
 				</div>
 
 			</table>
