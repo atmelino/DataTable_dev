922b8af596f2d0d77e67d05eae7b112ca2705272
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-24 20:53:51 -0500

    u

--- DataTable.tsx
+++ DataTable.tsx
@@ -15,6 +15,8 @@ export function DataTable(props: DataTableProps) {
 	const [currentPage, setcurrentPage] = useState(1);
 	const rowsPerPage = 15;
 	const pageNumbers: number[] = [];
+	const totalRows = props.dataArray.length;
+	const totalPages=Math.ceil(totalRows / rowsPerPage);
 
 
 	function showData() {
@@ -38,7 +40,6 @@ export function DataTable(props: DataTableProps) {
 	}
 
 	function showPagination() {
-		const totalRows = props.dataArray.length;
 		const inactive = "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
 		const active = "z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
 
@@ -106,7 +107,7 @@ export function DataTable(props: DataTableProps) {
 			</table>
 			{showPagination()}
 			<Pagino
-				count={1}
+				count={totalPages}
 				showFirst={true}
 				showPrevious={true}
 				showNext={true}
