d639cfef79662a1c5e079475eab92a4dd26dc1a4
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-28 19:32:18 -0500

    u

--- examples/longDataTable/islands/Body.tsx
+++ examples/longDataTable/islands/Body.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "preact/hooks";
 // import { DataTable } from "../../../../DataTable/mod.ts";
 // import { DataTable } from "https://raw.githubusercontent.com/atmelino/DataTable/main/mod.ts"
 import { DataTable } from "../../../../DataTable_dev/mod.ts";
+
 import MyData from "./myData.tsx";
 
 export default function Body() {
