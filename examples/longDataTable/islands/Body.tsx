14ac15d7c8be596a08938ae95a9a64155e8b0dc4
atmelino <you@example.com>
2023-05-31 23:53:22 -0500

    u

--- examples/longDataTable/islands/Body.tsx
+++ examples/longDataTable/islands/Body.tsx
@@ -5,7 +5,6 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import { DataTable } from "../../../../DataTable_dev/mod.ts";
 // import { DataTable } from "https://deno.land/x/datatable@v0.0.4-alpha/mod.ts";
 
-
 import MyData from "./myData.tsx";
 
 export default function Body() {
